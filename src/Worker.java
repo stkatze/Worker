e319411b0e888a2477475fe8b10367799ee69047
st.katze <st.katze@icloud.com>
2023-04-25 01:43:26 +0300

    Initial commit

---
+++ src/Worker.java
@@ -0,0 +1,20 @@
+public class Worker {
+
+    private OnTaskDoneListener callback;
+    private OnTaskErrorListener errorCallback;
+
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+        this.callback = callback;
+        this.errorCallback = errorCallback;
+    }
+
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            if (i != 33) {
+                callback.onDone("Task " + i + " is done");
+            } else {
+                errorCallback.onError("Error! Task " + i + " isn't done");
+            }
+        }
+    }
+}
